b1c6c9dc4479468151a1c43220fabd2824bd95a1
Chris Cheshire <chris.cheshire@kelkooventures.com>
2011-07-19 16:43:45 +0100

    updated db details

--- lib/models.rb
+++ lib/models.rb
@@ -1,11 +1,20 @@
 require 'active_record'
+require 'uri'
 #require 'redcloth'
 #require 'syntaxi'
 
+db = URI.parse(ENV['DATABASE_URL'] || 'sqlite3://localhost/lib/frog.db')
+
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "lib/frog.db"
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
 )
+
+
 
 class Blog < ActiveRecord::Base
   has_many :entries, :order => 'created_at DESC'
